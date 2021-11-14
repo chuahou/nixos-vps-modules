cee0fe59307326223e512021769967774dcfb964
Chua Hou <human+github@chuahou.dev>
2021-11-14 16:58:13 +0000

    vps-common: remove alignment spaces

--- modules/vps-common.nix
+++ modules/vps-common.nix
@@ -17,18 +17,18 @@
     # Start headless without serial tty.
     systemd.services = {
       "serial-getty@ttyS0".enable = false;
-      "serial-getty@hvc0".enable  = false;
-      "getty@tty1".enable         = false;
-      "autovt@".enable            = false;
+      "serial-getty@hvc0".enable = false;
+      "getty@tty1".enable = false;
+      "autovt@".enable = false;
     };
 
     # Enable SSH.
     networking.firewall.allowedTCPPorts = [ 22 ];
-    services.sshd.enable                = true;
+    services.sshd.enable = true;
 
     # Enable Nix flakes.
     nix = {
-      package      = pkgs.nixUnstable;
+      package = pkgs.nixUnstable;
       extraOptions = "experimental-features = nix-command flakes";
     };
   };
