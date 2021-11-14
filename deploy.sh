194fa606c247b70dfcbd137a939fcf8234ebd94d
Chua Hou <human+github@chuahou.dev>
2021-11-14 17:10:20 +0000

    deploy.sh: init deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Chua Hou
+#
+# Builds host $1, copies closure to $2 and activates it by switch or $3.
+
+set -euo pipefail
+
+nixos-rebuild build --flake $(dirname $0)#$1
+nix-copy-closure --to -v root@$2 $(realpath result)
+ssh root@$2 $(realpath result)/bin/switch-to-configuration ${3:-switch}
--- flake.nix
+++ flake.nix
@@ -4,7 +4,9 @@
   outputs = inputs@{ self, nixpkgs }:
   let
     system = "x86_64-linux";
-    inherit (nixpkgs.legacyPackages.${system}) lib;
+    pkgs = nixpkgs.legacyPackages.${system};
+    inherit (pkgs) lib;
+
     modules = let dir = ./modules; in builtins.map (file: dir + "/${file}")
       (builtins.attrNames (builtins.readDir dir));
     imported = builtins.map (file: {
@@ -14,5 +16,8 @@
   in rec {
     nixosModules = builtins.foldl' (x: y: x // y) {} imported;
     allModules = builtins.attrValues nixosModules;
+
+    deployScript = pkgs.writeShellScriptBin "deploy.sh"
+      (builtins.readFile ./deploy.sh);
   };
 }
